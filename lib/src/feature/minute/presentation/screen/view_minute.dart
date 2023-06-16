91ff051df1ae16799e1912600c27d2b28d5ede96
André Meireles <andre2meireles@gmail.com>
2023-06-16 16:49:37 -0300

    show messages

--- lib/src/feature/minute/presentation/screen/view_minute.dart
+++ lib/src/feature/minute/presentation/screen/view_minute.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:atas/src/core/core.dart';
 import 'package:atas/src/feature/minute/minute.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 
@@ -53,22 +54,100 @@ class _ViewMinuteState extends State<ViewMinute> {
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          const Text('Reunião Sacramental'),
+          _leadingText('Reunião Sacramental'),
           Row(
-            children: [
-              Text('Data: ${DateFormat('d/MM/y').format(_minute.date)}'),
-            ],
+            children: [Text('Data: ${DateFormat('d/MM/y').format(_minute.date)}')],
           ),
-          const Text('Reconhecimentos'),
-          Column(
-            children: _getMultiples(Label.recognition, Types.call).map((e) {
-              final c = e as Call;
-              return Text('${c.name} - ${c.call}');
-            }).toList(),
-          )
+          _callRow(label: Label.presiding),
+          _callRow(label: Label.driving),
+          if (_getMultiples(Label.recognition, Types.call).isNotEmpty)
+            ..._multipleRow(name: 'Reconhecimento', label: Label.recognition, type: Types.call),
+          if (_getMultiples(Label.announcement, Types.simpleText).isNotEmpty)
+            ..._multipleRow(name: 'Anúncios', label: Label.announcement, type: Types.simpleText),
+          _hymnRow(label: Label.firstHymn, placeholderText: 'primeiro hino'),
+          _simpleRow(label: Label.fistPray, placeholderText: 'primeira oração'),
+          if (_getMultiples(Label.call, Types.call).isNotEmpty)
+            ..._multipleRow(name: 'Chamados', label: Label.call, type: Types.call),
+          if (_getMultiples(Label.callRelease, Types.call).isNotEmpty)
+            ..._multipleRow(name: 'Desobrigação', label: Label.callRelease, type: Types.call),
+          _hymnRow(label: Label.sacramentalHym, placeholderText: 'hino sacramental'),
+          _leadingText('Sacramento'),
+          _simpleRow(label: Label.firstSpeaker, placeholderText: 'primeiro orador'),
+          _simpleRow(label: Label.secondSpeaker),
+          _hymnRow(label: Label.intermediaryHym, placeholderText: 'Hino Intermediário'),
+          _simpleRow(label: Label.thirdSpeaker, placeholderText: 'ultimo orador'),
+          _hymnRow(label: Label.endingHymn, placeholderText: 'hino de encerramento'),
+          _simpleRow(label: Label.endingPray, placeholderText: 'oração de encerramento'),
         ],
       ),
     );
+  }
+
+  _leadingText(String text) => Text(text, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15));
+
+  Widget _callRow({required Label label, String? placeholderText, int size = 30}) {
+    final call = _getSingleCall(label);
+    if (call == null) return Text(placeholderText ?? 'preencher');
+    final textArea = translate(call.label.value);
+    return Flexible(
+      child: ListTile(
+        leading: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('$textArea:'.padRight(size - textArea.length))],
+        ),
+        title: Text(call.name),
+        trailing: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text(call.call)]),
+      ),
+    );
+  }
+
+  Widget _simpleRow({required Label label, String? placeholderText, int size = 30}) {
+    final simple = _getSingleSimple(label);
+    if (simple == null) return Text(placeholderText ?? 'preencher');
+    final text = translate(simple.label.value);
+    return Flexible(
+      child: ListTile(
+        leading: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('$text:'.padRight(size - text.length))],
+        ),
+        title: Text(simple.value),
+      ),
+    );
+  }
+
+  _hymnRow({required Label label, String? placeholderText, int size = 30}) {
+    final hymn = _getSingleHymn(label);
+    if (hymn == null) return Text(placeholderText ?? 'preencher');
+    final text = translate(hymn.label.value);
+    return Flexible(
+      child: ListTile(
+        leading: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('$text:'.padRight(size - text.length))],
+        ),
+        title: Text(hymn.name),
+        trailing: Text('Nº ${hymn.number}'),
+      ),
+    );
+  }
+
+  List<Widget> _multipleRow({required String name, required Label label, required Types type}) {
+    return [
+      _leadingText(name),
+      SizedBox(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: _getMultiples(label, type).map((e) {
+            if (e.type == Types.call) {
+              return _callRow(label: e.label);
+            }
+            return _simpleRow(label: e.label);
+          }).toList(),
+        ),
+      ),
+      const Divider(thickness: 2),
+    ];
   }
 
   get _load => const Center(child: CircularProgressIndicator());
@@ -76,4 +155,16 @@ class _ViewMinuteState extends State<ViewMinute> {
   List<Assign> _getMultiples(Label label, Types type) {
     return _minute.assignments.where((e) => e.label == label && e.type == type).toList();
   }
+
+  Assign? _getSingle(Label label, Types type) {
+    final results = _minute.assignments.where((e) => e.label == label && e.type == type);
+    if (results.isEmpty) return null;
+    return results.first;
+  }
+
+  Call? _getSingleCall(Label label) => _getSingle(label, Types.call) as Call?;
+
+  Hymn? _getSingleHymn(Label label) => _getSingle(label, Types.hymn) as Hymn?;
+
+  SimpleText? _getSingleSimple(Label label) => _getSingle(label, Types.simpleText) as SimpleText?;
 }
